edb5ef6e4af1d57fc3f65e179af9eb670e609898
Srivathsan Ganesan <srivathsan18901@gmail.com>
2026-03-19 14:43:03 +0530

    initial changes

---
+++ VisioNeo.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace VisioNeo_App
+{
+    partial class VisioNeo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VisioNeo));
+            panel1 = new Panel();
+            MaximizeBTN = new PictureBox();
+            CloseBTN = new PictureBox();
+            MinimizeBTN = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            VisualPB = new PictureBox();
+            panel2 = new Panel();
+            devListTBox = new ListBox();
+            CnctBTN = new Button();
+            LoadingPB = new PictureBox();
+            searchBTN = new MaterialSkin.Controls.MaterialButton();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)MaximizeBTN).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)CloseBTN).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)MinimizeBTN).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)VisualPB).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)LoadingPB).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.BackColor = Color.Transparent;
+            panel1.Controls.Add(MaximizeBTN);
+            panel1.Controls.Add(CloseBTN);
+            panel1.Controls.Add(MinimizeBTN);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(-1, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1649, 74);
+            panel1.TabIndex = 0;
+            // 
+            // MaximizeBTN
+            // 
+            MaximizeBTN.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            MaximizeBTN.BackColor = Color.Transparent;
+            MaximizeBTN.Image = (Image)resources.GetObject("MaximizeBTN.Image");
+            MaximizeBTN.Location = new Point(1571, 22);
+            MaximizeBTN.Name = "MaximizeBTN";
+            MaximizeBTN.Size = new Size(25, 25);
+            MaximizeBTN.SizeMode = PictureBoxSizeMode.StretchImage;
+            MaximizeBTN.TabIndex = 6;
+            MaximizeBTN.TabStop = false;
+            MaximizeBTN.Click += MaximizeBTN_Click;
+            // 
+            // CloseBTN
+            // 
+            CloseBTN.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            CloseBTN.BackColor = Color.Transparent;
+            CloseBTN.Image = (Image)resources.GetObject("CloseBTN.Image");
+            CloseBTN.Location = new Point(1613, 22);
+            CloseBTN.Name = "CloseBTN";
+            CloseBTN.Size = new Size(25, 25);
+            CloseBTN.SizeMode = PictureBoxSizeMode.StretchImage;
+            CloseBTN.TabIndex = 3;
+            CloseBTN.TabStop = false;
+            CloseBTN.Click += CloseBTN_Click;
+            // 
+            // MinimizeBTN
+            // 
+            MinimizeBTN.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            MinimizeBTN.BackColor = Color.Transparent;
+            MinimizeBTN.Image = (Image)resources.GetObject("MinimizeBTN.Image");
+            MinimizeBTN.Location = new Point(1528, 22);
+            MinimizeBTN.Name = "MinimizeBTN";
+            MinimizeBTN.Size = new Size(25, 25);
+            MinimizeBTN.SizeMode = PictureBoxSizeMode.StretchImage;
+            MinimizeBTN.TabIndex = 5;
+            MinimizeBTN.TabStop = false;
+            MinimizeBTN.Click += MinimizeBTN_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(95, 22);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(128, 39);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(9, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(80, 51);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // VisualPB
+            // 
+            VisualPB.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            VisualPB.BackColor = Color.Transparent;
+            VisualPB.Image = (Image)resources.GetObject("VisualPB.Image");
+            VisualPB.Location = new Point(3, 53);
+            VisualPB.Name = "VisualPB";
+            VisualPB.Size = new Size(1028, 633);
+            VisualPB.SizeMode = PictureBoxSizeMode.StretchImage;
+            VisualPB.TabIndex = 1;
+            VisualPB.TabStop = false;
+            VisualPB.Click += VisualPB_Click;
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
+            panel2.BackgroundImageLayout = ImageLayout.Stretch;
+            panel2.Controls.Add(devListTBox);
+            panel2.Controls.Add(CnctBTN);
+            panel2.Controls.Add(LoadingPB);
+            panel2.Controls.Add(searchBTN);
+            panel2.Controls.Add(VisualPB);
+            panel2.Location = new Point(-1, 76);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1034, 689);
+            panel2.TabIndex = 2;
+            // 
+            // devListTBox
+            // 
+            devListTBox.BackColor = Color.White;
+            devListTBox.BorderStyle = BorderStyle.None;
+            devListTBox.Font = new Font("Arial Narrow", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            devListTBox.ForeColor = SystemColors.Highlight;
+            devListTBox.FormattingEnabled = true;
+            devListTBox.Location = new Point(149, 11);
+            devListTBox.Name = "devListTBox";
+            devListTBox.Size = new Size(306, 32);
+            devListTBox.TabIndex = 10;
+            // 
+            // CnctBTN
+            // 
+            CnctBTN.BackColor = Color.White;
+            CnctBTN.FlatStyle = FlatStyle.Flat;
+            CnctBTN.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            CnctBTN.ForeColor = Color.SeaGreen;
+            CnctBTN.Location = new Point(461, 6);
+            CnctBTN.Name = "CnctBTN";
+            CnctBTN.Size = new Size(129, 39);
+            CnctBTN.TabIndex = 9;
+            CnctBTN.Text = "CONNECT";
+            CnctBTN.UseVisualStyleBackColor = false;
+            CnctBTN.Click += CnctBTN_Click_1;
+            // 
+            // LoadingPB
+            // 
+            LoadingPB.BackColor = Color.Transparent;
+            LoadingPB.Image = (Image)resources.GetObject("LoadingPB.Image");
+            LoadingPB.Location = new Point(596, 5);
+            LoadingPB.Name = "LoadingPB";
+            LoadingPB.Size = new Size(39, 41);
+            LoadingPB.SizeMode = PictureBoxSizeMode.StretchImage;
+            LoadingPB.TabIndex = 7;
+            LoadingPB.TabStop = false;
+            LoadingPB.Click += LoadingPB_Click;
+            // 
+            // searchBTN
+            // 
+            searchBTN.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            searchBTN.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            searchBTN.Depth = 0;
+            searchBTN.HighEmphasis = true;
+            searchBTN.Icon = null;
+            searchBTN.Location = new Point(4, 8);
+            searchBTN.Margin = new Padding(4, 6, 4, 6);
+            searchBTN.MouseState = MaterialSkin.MouseState.HOVER;
+            searchBTN.Name = "searchBTN";
+            searchBTN.NoAccentTextColor = Color.Empty;
+            searchBTN.Size = new Size(141, 36);
+            searchBTN.TabIndex = 2;
+            searchBTN.Text = "Search Vision";
+            searchBTN.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            searchBTN.UseAccentColor = false;
+            searchBTN.UseVisualStyleBackColor = true;
+            searchBTN.Click += searchBTN_Click;
+            // 
+            // VisioNeo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1649, 766);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "VisioNeo";
+            Text = "VisioNeo";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)MaximizeBTN).EndInit();
+            ((System.ComponentModel.ISupportInitialize)CloseBTN).EndInit();
+            ((System.ComponentModel.ISupportInitialize)MinimizeBTN).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)VisualPB).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)LoadingPB).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox CloseBTN;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private PictureBox MinimizeBTN;
+        private PictureBox MaximizeBTN;
+        private PictureBox VisualPB;
+        private Panel panel2;
+        private MaterialSkin.Controls.MaterialButton searchBTN;
+        private PictureBox LoadingPB;
+        private Button CnctBTN;
+        private ListBox devListTBox;
+    }
+}
---
+++ VisioNeo.cs
@@ -0,0 +1,280 @@
+﻿namespace VisioNeo_App
+{
+    using MvCamCtrl.NET;
+    using System.Drawing.Imaging;
+    using System.Runtime.InteropServices;
+
+    public partial class VisioNeo : Form
+    {
+
+        MyCamera camera = new MyCamera();
+        MyCamera.MV_CC_DEVICE_INFO_LIST deviceList;
+        Thread grabThread;
+        bool isGrabbing = false;
+        bool isConnected = false;
+
+
+        public VisioNeo()
+        {
+            InitializeComponent();
+            LoadingPB.Visible = false;
+            CnctBTN.Visible = false;
+            devListTBox.Visible = false;
+            VisualPB.Image = Properties.Resources.No_Data_Founds;
+            VisualPB.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void MinimizeBTN_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void MaximizeBTN_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void CloseBTN_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private async void searchBTN_Click(object sender, EventArgs e)
+        {
+            // Step 1: Show loader
+            LoadingPB.Visible = true;
+            CnctBTN.Visible = false;
+            devListTBox.Items.Clear();
+
+            // Step 2: Run camera search in background
+            var devices = await Task.Run(() => SearchCamera());
+
+            // Step 3: Update UI after search
+            LoadingPB.Visible = false;
+
+            if (devices.Count > 0)
+            {
+                foreach (var dev in devices)
+                {
+                    devListTBox.Items.Add(dev);
+                }
+                devListTBox.Visible = true;
+                CnctBTN.Visible = true; // show connect button
+            }
+
+            else
+            {
+                devListTBox.Items.Clear();
+                devListTBox.Items.Add("No camera found");
+            }
+        }
+
+        private List<string> SearchCamera()
+        {
+            List<string> devices = new List<string>();
+
+            deviceList = new MyCamera.MV_CC_DEVICE_INFO_LIST();
+
+            int result = MyCamera.MV_CC_EnumDevices_NET(
+                MyCamera.MV_GIGE_DEVICE | MyCamera.MV_USB_DEVICE,
+                ref deviceList);
+
+            if (result != MyCamera.MV_OK)
+            {
+                return devices;
+            }
+
+            for (int i = 0; i < deviceList.nDeviceNum; i++)
+            {
+                MyCamera.MV_CC_DEVICE_INFO deviceInfo =
+                    (MyCamera.MV_CC_DEVICE_INFO)Marshal.PtrToStructure(
+                        deviceList.pDeviceInfo[i],
+                        typeof(MyCamera.MV_CC_DEVICE_INFO));
+
+                if (deviceInfo.nTLayerType == MyCamera.MV_GIGE_DEVICE)
+                {
+                    var gigeInfo = (MyCamera.MV_GIGE_DEVICE_INFO)
+                        MyCamera.ByteToStruct(deviceInfo.SpecialInfo.stGigEInfo,
+                        typeof(MyCamera.MV_GIGE_DEVICE_INFO));
+
+                    string ip = $"{(gigeInfo.nCurrentIp & 0xff)}." +
+                                $"{((gigeInfo.nCurrentIp >> 8) & 0xff)}." +
+                                $"{((gigeInfo.nCurrentIp >> 16) & 0xff)}." +
+                                $"{((gigeInfo.nCurrentIp >> 24) & 0xff)}";
+
+                    string model = gigeInfo.chModelName?.Trim('\0');
+                    string name = gigeInfo.chUserDefinedName?.Trim('\0');
+
+                    devices.Add($"Hikvision - {model} - {name} ({ip})");
+                }
+            }
+
+            return devices;
+        }
+
+        private void LoadingPB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VisualPB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CnctBTN_Click_1(object sender, EventArgs e)
+        {
+            // =========================
+            // DISCONNECT MODE
+            // =========================
+            if (isConnected)
+            {
+                try
+                {
+                    isConnected = false;
+                    isGrabbing = false;
+
+                    grabThread?.Join(500);
+
+                    camera.MV_CC_StopGrabbing_NET();
+                    camera.MV_CC_CloseDevice_NET();
+                    camera.MV_CC_DestroyDevice_NET();
+
+                    // Show GIF
+                    VisualPB.Image = new Bitmap(Properties.Resources.No_Data_Founds);
+
+                    ImageAnimator.Animate(VisualPB.Image, (s, ev) =>
+                    {
+                        VisualPB.Invalidate();
+                    });
+
+                    // UI Update
+                    CnctBTN.Text = "CONNECT";
+                    CnctBTN.ForeColor = Color.SeaGreen;
+
+                    isConnected = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                return;
+            }
+
+            // =========================
+            // CONNECT MODE
+            // =========================
+
+            int index = devListTBox.SelectedIndex;
+
+            if (index < 0)
+            {
+                MessageBox.Show("Please select a device!");
+                return;
+            }
+
+            try
+            {
+                MyCamera.MV_CC_DEVICE_INFO deviceInfo =
+                    (MyCamera.MV_CC_DEVICE_INFO)Marshal.PtrToStructure(
+                        deviceList.pDeviceInfo[index],
+                        typeof(MyCamera.MV_CC_DEVICE_INFO));
+
+                int result;
+
+                result = camera.MV_CC_CreateDevice_NET(ref deviceInfo);
+                if (result != MyCamera.MV_OK)
+                {
+                    MessageBox.Show("Create handle failed");
+                    return;
+                }
+
+                result = camera.MV_CC_OpenDevice_NET();
+                if (result != MyCamera.MV_OK)
+                {
+                    MessageBox.Show("Open device failed");
+                    return;
+                }
+
+                camera.MV_CC_SetEnumValue_NET("TriggerMode", 0);
+
+                camera.MV_CC_StartGrabbing_NET();
+
+                isGrabbing = true;
+                grabThread = new Thread(GrabLoop);
+                grabThread.IsBackground = true;
+                grabThread.Start();
+
+                // UI Update
+                CnctBTN.Text = "DISCONNECT";
+                CnctBTN.ForeColor = Color.Red;
+
+                isConnected = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void GrabLoop()
+        {
+            MyCamera.MV_FRAME_OUT frame = new MyCamera.MV_FRAME_OUT();
+
+            while (isGrabbing)
+            {
+                int result = camera.MV_CC_GetImageBuffer_NET(ref frame, 1000);
+
+                if (result == MyCamera.MV_OK)
+                {
+                    try
+                    {
+                        int width = frame.stFrameInfo.nWidth;
+                        int height = frame.stFrameInfo.nHeight;
+
+                        byte[] buffer = new byte[frame.stFrameInfo.nFrameLen];
+                        Marshal.Copy(frame.pBufAddr, buffer, 0, buffer.Length);
+
+                        Bitmap bmp = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+
+                        // Set grayscale palette
+                        ColorPalette palette = bmp.Palette;
+                        for (int i = 0; i < 256; i++)
+                        {
+                            palette.Entries[i] = Color.FromArgb(i, i, i);
+                        }
+                        bmp.Palette = palette;
+
+                        BitmapData bmpData = bmp.LockBits(
+                            new Rectangle(0, 0, width, height),
+                            ImageLockMode.WriteOnly,
+                            bmp.PixelFormat);
+
+                        Marshal.Copy(buffer, 0, bmpData.Scan0, buffer.Length);
+                        bmp.UnlockBits(bmpData);
+
+                        VisualPB.Invoke(new Action(() =>
+                        {
+                            if (!isConnected) return; // 🔥 IMPORTANT FIX
+
+                            VisualPB.Image?.Dispose();
+                            VisualPB.Image = bmp;
+                        }));
+                    }
+                    catch { }
+
+                    camera.MV_CC_FreeImageBuffer_NET(ref frame);
+                }
+            }
+        }
+
+    }
+}
